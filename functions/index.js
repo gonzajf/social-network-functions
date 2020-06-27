84b3179daaf52971457a033926fe1a938b10861d
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-06-27 18:11:16 -0300

    middleware to protect endpoints to be authenticated

--- functions/index.js
+++ functions/index.js
@@ -47,11 +47,43 @@ app.get('/posts', (request, response) => {
 
 });
 
-app.post('/posts', (request, response) => {
+
+const FBAuth = (request, response, next) => {
+
+    let idToken;
+
+    if(request.headers.authorization && request.headers.authorization.startsWith('Bearer ')) {
+        idToken = request.headers.authorization.split('Bearer ')[1];
+    } 
+    else {
+        console.error('No token found');
+        
+        return response.status(403).json({ error: 'Unauthorized' });
+    }
+
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            request.user = decodedToken;
+            return db.collection('users')
+                    .where('userId', "==", request.user.uid)
+                    .limit(1)
+                    .get();
+        })
+        .then(data => {
+            request.user.userHandle = data.docs[0].data().userHandle;
+            return next();
+        })
+        .catch(error => {
+            console.error('Error while verifying token');
+            return response.status(403).json(error);
+        });
+}
+
+app.post('/posts', FBAuth, (request, response) => {
   
     const newPost = {
         body: request.body.body,
-        userHandle: request.body.userHandle,
+        userHandle: request.user.userHandle,
         createdAt: new Date().toISOString()
     };
 
