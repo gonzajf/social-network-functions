a4dc0f8cd7ea3b7bffd63edecece59894e358762
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-07-26 15:44:30 -0300

    small fix

--- functions/index.js
+++ functions/index.js
@@ -117,13 +117,13 @@ exports.onPostDelete = functions.firestore.document('/posts/{postId}')
                                 data.forEach(doc => {
                                         batch.delete(db.doc(`/comments/${doc.id}`)); 
                                 })
-                                return db.collection('likes').where('postId', '==', postId);
+                                return db.collection('likes').where('postId', '==', postId).get();
                         })
                         .then(data => {
                                 data.forEach(doc => {
                                         batch.delete(db.doc(`/likes/${doc.id}`)); 
                                 })
-                                return db.collection('notifications').where('postId', '==', postId);
+                                return db.collection('notifications').where('postId', '==', postId).get();
                         })
                         .then(data => {
                                 data.forEach(doc => {
